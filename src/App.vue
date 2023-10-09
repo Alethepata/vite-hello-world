82038077875817e3468a7f4c5489e9d51642b107
Alethepata <alessia.patane@gmail.com>
2023-10-09 16:06:02 +0200

    [Edit] script(AppHero)/ template/ style

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <script>
+
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   name: 'App',
   components:{
-    AppTitle
+    AppTitle,
+    AppHero
   }
 }
 </script>
 
 <template>
 
-  <AppTitle/>
+
+  <section>
+
+    <AppTitle/>  
+
+  </section>
+
+  <section class="py-5 mt-3 ">
+
+    <AppHero/>
+
+  </section>
+
   
 </template>
 
 <style>
+section:nth-child(2){
+  background: linear-gradient(118deg, rgba(9,9,121,1) 0%, rgba(20,71,221,1) 40%, rgba(0,212,255,1) 100%);
+  color: white; 
+}
 
 </style>
