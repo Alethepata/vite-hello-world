60f225141abec550184cdb2063145fd1d5030803
Alethepata <alessia.patane@gmail.com>
2023-10-09 16:04:52 +0200

    [Add] AppHero script/ template/style

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name:'AppHero'
+}
+</script>
+
+<template>
+<div class="container_jumbotron">
+    <h1>Jumbotron</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod enim ipsa dolorem animi voluptas sequi?</p>
+    <button class="btn btn-primary fs-5">Button</button>
+</div>
+</template>
+
+<style>
+.container_jumbotron{
+    margin: 50px auto;
+    width: 80%;
+    font-size: 25px;
+}
+.container_jumbotron h1{
+    font-size: 50px;
+}
+</style>
